6ae22ef7e86493ee4a7c22d7b72b15c896bd8b79
junwoo kim <bababrll@naver.com>
2018-05-27 04:35:16 +0900

    add reverse_item

--- Tetris_C Language/main.c
+++ Tetris_C Language/main.c
@@ -5,35 +5,35 @@
 
 int main(void)
 {
-	BLOCK* block;
+   BLOCK* block;
+   int mode = Intro();
 
+   if (mode == 1) // origin mode
+   {
+      system("cls");
+      block = first_block(); //make first block
+      fill_screen();// make the tetris map
+      show_screen();// make all thing 
 
-	if (Intro() == 1) // origin mode
-	{
-		system("cls");
-		block = first_block(); //make first block
-		fill_screen();// make the tetris map
-		show_screen();// make all thing 
+      run_game(block);// run the game
+   }
+   else if (mode == 2) // using Item mode
+   {
+      system("cls");
+      block = item_first_block(); //make first block
+      fill_screen();// make the tetris map
+      show_screen();// make all thing 
 
-		run_game(block);// run the game
-	}
-	else if (Intro() == 2) // using Item mode
-	{
-		system("cls");
-		block = first_block(); //make first block
-		fill_screen();// make the tetris map
-		show_screen();// make all thing 
+      run_itemgame(block);// run the game
+   }
+   else if (mode == 3) // show AI mode
+   {
+      system("cls");
+      block = first_block(); //make first block
+      fill_screen();// make the tetris map
+      show_screen();// make all thing 
 
-		run_itemgame(block);// run the game
-	}
-	else if (Intro() == 3) // show AI mode
-	{
-		system("cls");
-		block = first_block(); //make first block
-		fill_screen();// make the tetris map
-		show_screen();// make all thing 
-
-		run_game(block);// run the game
-	}
-	return 0;
+      run_game(block);// run the game
+   }
+   return 0;
 }
